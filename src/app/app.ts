38be267139e46ef2ab56b3974a529de4d08a9477
Jakub Cygan <jakub.cygan@gmail.com>
2025-08-24 14:43:28 +0200

    Tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  firestore = inject(Firestore);
+
   protected title = 'boards20';
+  // get a reference to the user-profile collection
+  userProfileCollection = collection(this.firestore, 'users');
+  users$ = collectionData(this.userProfileCollection) as Observable<Item[]>;
 }
